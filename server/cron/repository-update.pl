3b9e0691b37144fa45bb4a22e1c7ba4a2587f43d
Michal Jurosz <mj@mj41.cz>
2008-02-05 22:38:27 +0000

    import rep log to db - next step

--- server/cron/repository-update.pl
+++ server/cron/repository-update.pl
@@ -8,6 +8,7 @@ use Data::Dumper;
 use File::Spec::Functions;
 
 use SVN::Log;
+use Time::Local;
 
 my $conf_fpath = catfile( $RealBin, '..', 'conf', 'dbconf.pl' );
 my $conf = require $conf_fpath;
@@ -16,31 +17,22 @@ croak "Config loaded from '$conf_fpath' is empty.\n" unless $conf;
 my $project_name = 'parrot';
 my $conf_rep = $conf->{project}->{$project_name};
 
-my $state_fn = catfile( $RealBin,'..', 'conf', $project_name . '-replog-state.pl' );
-my $log_dump_fn = catfile( $RealBin, '..', 'conf', $project_name.'-replog-dump.pl' );
-
 my $log_dump_refresh = $ARGV[0] || $conf_rep->{log_dump_refresh};
 my $debug = $ARGV[1] || 0;
+my $debug_logpart = $ARGV[2] || 0;
 
-if ( 0 ) {
-    my $dbh = DBI->connect(
-        $conf->{db}->{dsn},
-        $conf->{db}->{user},
-        $conf->{db}->{password},
-        { RaiseError => 1, AutoCommit => 0 }
-    ) or die $DBI::errstr;
+my $state_fn = catfile( $RealBin,'..', 'conf', $project_name . '-replog-state.pl' );
+my $log_dump_file = $project_name.'-replog-dump.pl';
+$log_dump_file = $project_name.'-replog-debugdump.pl' if $debug_logpart;
+my $log_dump_fn = catfile( $RealBin, '..', 'conf', $log_dump_file );
 
-
-    my $sth = $dbh->prepare("SELECT client_id, user_id, created, last_login, ip, cpuarch, osname, archname, active  FROM client WHERE active=1");
-    $sth->execute();
-
-    while ( my @row = $sth->fetchrow_array ) {
-        print join(' | ',@row) . "\n";
-    }
-
-    $dbh->commit;
-    $dbh->disconnect;
-}
+my $dbh;
+my $dbh = DBI->connect(
+    $conf->{db}->{dsn},
+    $conf->{db}->{user},
+    $conf->{db}->{password},
+    { RaiseError => 1, AutoCommit => 0 }
+) or die $DBI::errstr;
 
 sub dmp {
     my $dd = Data::Dumper->new( [ @_ ] );
@@ -79,6 +71,7 @@ else {
     $state->{project_name} = $project_name;
     $state->{create_time} = time();
     $state->{log_dump_time} => undef;
+    $state->{rev_to_db_saved} => undef;
     save_state();
 }
 
@@ -96,11 +89,16 @@ else {
 }
 print "last_log_rev: $last_log_rev\n" if $debug > 1;
 
- 
+
 # each $log_dump_refresh hours add fresh log info
-if ( $state->{rep}->{log_dump_time} + $log_dump_refresh < time() ) {
-    print "getting svn log for revisions $last_log_rev..HEAD online...\n";
-    my $new_revs = SVN::Log::retrieve ($conf_rep->{repository}, $last_log_rev, 'HEAD');
+if ( ( !$debug_logpart || (not -e $log_dump_fn) )
+     && $state->{rep}->{log_dump_time} + $log_dump_refresh < time()
+   )
+{
+    my $to_rev = 'HEAD';
+    $to_rev = '100' if $debug_logpart;
+    print "getting svn log for revisions $last_log_rev..$to_rev online...\n";
+    my $new_revs = SVN::Log::retrieve ($conf_rep->{repository}, $last_log_rev, $to_rev);
     
     #shift @$new_revs;
     #print dmp( $new_revs );
@@ -125,19 +123,44 @@ if ( $state->{rep}->{log_dump_time} + $log_dump_refresh < time() ) {
 }
 
 
+sub fatal_error {
+    my ( $dbh, $rd, $msg ) = @_;
+    
+    $dbh->rollback;
+    $dbh->disconnect;
+    croak 
+        $msg
+        . "\n"
+        . "For revison " . $rd->{revisons} . ".\n"
+    ;
+}
+
+
+#my $sth = $dbh->prepare("INSERT INTO rev, bar FROM table WHERE baz=?");
 
 foreach my $rd ( @$revs ) {
     my $found = 0;
+    
+    # 2001-09-15T20:51:23.000000Z
+    my $date_ts = 0;
+    if ( my ($year,$mon,$mday,$hour,$min,$sec) = $rd->{date} =~
+     /^(\d{4})\-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})\.\d+Z$/ ) {
+        $date_ts = timegm($sec,$min,$hour,$mday,$mon,$year);
+    }
+    else {
+        fatal_error( $dbh, $rd, "Time parser error date str '".$rd->{date}."'." );
+    }
 
     if ( $debug > 1 ) {
         print dmp($rd) if $debug > 4;
         print "rev:" . $rd->{'revision'};
         print ", autor: " . $rd->{'author'};
-        print ", datum: " . $rd->{'date'};
+        print ", date: " . $rd->{'date'};
         print "\n";
         my $msg = $rd->{'message'};
         print "msg:\n";
         $msg =~ s{\r}{}sg;
+        $msg =~ s{^\n}{}sg;
         $msg =~ s{\n+$}{}sg;
         $msg = '  ' . $msg;
         $msg =~ s{\n}{\n  }sg;
@@ -149,5 +172,10 @@ foreach my $rd ( @$revs ) {
         }
     }
     
+    #$state->{rev_to_db_saved} = $rd->{revisions};
+    # save_state();
     
 }
+
+$dbh->commit;
+$dbh->disconnect;
