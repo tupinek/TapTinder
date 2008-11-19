e204b047bf32725a0a7b6cbba42a956cd93fd7d9
Michal Jurosz <mj@mj41.cz>
2008-11-19 21:10:08 +0000

    cron loop.sh - add tmp dir path

--- server/cron/loop.sh
+++ server/cron/loop.sh
@@ -2,7 +2,7 @@ for ((i=1;1;i++)); do
 echo "Run number: " $i
 date
 echo ""
-nice -n 10 perl repository-update.pl && nice -n 10 perl tests-to-db.pl 
+nice -n 10 perl repository-update.pl && nice -n 10 perl tests-to-db.pl /tmp/taptinder/upload
 echo "Sleeping for 300 s ..."
 sleep 300
 echo ""
