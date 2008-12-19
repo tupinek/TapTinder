f253e381052ca334d161974691e07681e8ae6171
Michal Jurosz <mj@mj41.cz>
2008-12-19 21:11:53 +0000

    client/utils/up.pl - refactored for Windows

--- client/utils/up.pl
+++ client/utils/up.pl
@@ -1,14 +1,43 @@
-#!/usr/bin/perl
+#!perl
 
-$ENV{PERL5LIB} = '/home2/scripts/mj41/taptinder/libcpan';
+use strict;
+use warnings;
 
+use Carp qw(carp croak verbose);
+use FindBin qw($RealBin);
+
+use File::Spec::Functions;
+
+my $is_win32 = ( $^O eq 'MSWin32' );
+
+if ( $is_win32  ) {
+    $ENV{PERL5LIB} = '/home2/scripts/mj41/taptinder/libcpan';
+}
+
+my $dh;
 opendir( $dh, './' ) || die $!;
-
 while ( my $dir = readdir($dh) ) {
     print "dir: $dir\n";
     next if $dir eq '.' || $dir eq '..';
-    my $res_fp = $dir . '/taptinder-results.yaml';
-    system('/usr/bin/perl /home2/scripts/mj41/taptinder/client/upload.pl parrot ' . $res_fp . ' /home2/scripts/mj41/taptinder/client-conf/client-conf-ent.yaml');
+    next unless -d $dir;
+    my $res_fp = catfile( $dir,  'taptinder-results.yaml' );
+
+    my $base_cmd;
+    if ( $is_win32 ) {
+        $base_cmd = 'perl';
+    } else {
+        $base_cmd = '/usr/bin/perl';
+    }
+
+    my $cmd =
+        $base_cmd
+        . ' ' . catfile( $RealBin, '..', '..', 'client', 'upload.pl' )
+        . ' ' . 'parrot'
+        . ' ' . $res_fp
+        . ' ' . catfile( $RealBin, '..', '..', 'client-conf', 'client-conf.yaml' )
+    ;
+    system( $cmd );
+    #exit; # debug
 }
 
 close( $dh );
