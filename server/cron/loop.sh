632382dec567ca94827545f66672cac155c6b49e
Michal Jurosz <mj@mj41.cz>
2009-04-11 19:59:06 +0000

    server - repository update loop 300s -> 30s

--- server/cron/loop.sh
+++ server/cron/loop.sh
@@ -2,9 +2,9 @@ for ((i=1;1;i++)); do
 echo "Run number: " $i
 date
 echo ""
-nice -n 10 perl repository-update.pl -p Parrot && nice -n 10 perl tests-to-db.pl /tmp/taptinder/upload
-echo "Sleeping for 300 s ..."
-sleep 300
+nice -n 10 perl repository-update.pl -p Parrot
+echo "Sleeping for 30 s ..."
+sleep 30
 echo ""
 echo ""
 done
