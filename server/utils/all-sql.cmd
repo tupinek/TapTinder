11a759689260ef677eaba23060db7d0a14be29dd
Michal Jurosz <mj@mj41.cz>
2009-04-11 20:04:34 +0000

    server - utils - remove old server/utils/all-sql.cmd

--- server/utils/all-sql.cmd
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-echo Old version.
-exit 1;
-
-echo updating sql\schema.sql and temp\all.sql ...
-perl utils\wiki_schema.pl sql\schema.wiki > temp\schema.sql && type temp\schema.sql > temp\all.sql && type sql\data-base.sql >> temp\all.sql && echo done
-echo temp\sel-tables.sql (with trun table) ...
-perl utils\wiki_schema.pl sql\schema.wiki 0 0 0 trun > temp\sel-tables.sql && echo done
