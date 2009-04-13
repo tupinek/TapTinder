c8b6b02de6d9e98068ade9f3f463ed4c60a5839e
Michal Jurosz <mj@mj41.cz>
2009-04-13 18:59:39 +0000

    server - Catalyst scripts upgrade

--- server/script/taptinder_web_fastcgi.pl
+++ server/script/taptinder_web_fastcgi.pl
@@ -12,7 +12,7 @@ use TapTinder::Web;
 
 my $help = 0;
 my ( $listen, $nproc, $pidfile, $manager, $detach, $keep_stderr );
- 
+
 GetOptions(
     'help|?'      => \$help,
     'listen|l=s'  => \$listen,
@@ -25,10 +25,10 @@ GetOptions(
 
 pod2usage(1) if $help;
 
-TapTinder::Web->run( 
-    $listen, 
+TapTinder::Web->run(
+    $listen,
     {   nproc   => $nproc,
-        pidfile => $pidfile, 
+        pidfile => $pidfile,
         manager => $manager,
         detach  => $detach,
 	keep_stderr => $keep_stderr,
@@ -44,7 +44,7 @@ taptinder_web_fastcgi.pl - Catalyst FastCGI
 =head1 SYNOPSIS
 
 taptinder_web_fastcgi.pl [options]
- 
+
  Options:
    -? -help      display this help and exits
    -l -listen    Socket path to listen on
@@ -67,10 +67,9 @@ taptinder_web_fastcgi.pl [options]
 
 Run a Catalyst application as fastcgi.
 
-=head1 AUTHOR
+=head1 AUTHORS
 
-Sebastian Riedel, C<sri@oook.de>
-Maintained by the Catalyst Core Team.
+Catalyst Contributors, see Catalyst.pm
 
 =head1 COPYRIGHT
 
