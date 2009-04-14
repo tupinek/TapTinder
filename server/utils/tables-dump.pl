259a968ff1d0a849f1bf817264aefaf9da9d26ff
Michal Jurosz <mj@mj41.cz>
2009-04-14 17:23:00 +0000

    server/utils/tables-dump.pl refactore var names

--- server/utils/tables-dump.pl
+++ server/utils/tables-dump.pl
@@ -14,36 +14,36 @@ use TapTinder::Utils::Cmd qw(run_cmd_ipc);
 
 
 my $tables = $ARGV[0] || croak("Tables param is mandatory");
-my $conf_fpath  = $ARGV[1] || catfile( $RealBin, '..', '..', '..', 'tt', 'server', 'conf', 'web_db.yml' );
+my $src_conf_fpath  = $ARGV[1] || catfile( $RealBin, '..', '..', '..', 'tt', 'server', 'conf', 'web_db.yml' );
 my $use_root_passwd = $ARGV[2] || 0;
 my $noipc = $ARGV[3] || 1;
 # TODO dump_only load_only
 
-croak "DB conf file '$conf_fpath' not found." unless -f $conf_fpath;
+croak "DB conf file '$src_conf_fpath' not found." unless -f $src_conf_fpath;
 
 if ( $use_root_passwd && $noipc ) {
     croak "Parameters conflict: use_root_passwd=1 and noipc=1.\n";
 }
 
-my ( $conf ) = YAML::LoadFile( $conf_fpath );
-croak "Configuration for source database loaded from '$conf_fpath' is empty.\n" unless $conf->{db};
+my ( $src_conf ) = YAML::LoadFile( $src_conf_fpath );
+croak "Configuration for source database loaded from '$src_conf_fpath' is empty.\n" unless $src_conf->{db};
 
 my $dump_fpath = './temp/tables-dump.sql';
 
 my $cmd_dump = 'mysqldump';
-$cmd_dump .= ' -u ' . $conf->{db}->{user} unless $use_root_passwd;
+$cmd_dump .= ' -u ' . $src_conf->{db}->{user} unless $use_root_passwd;
 if ( $noipc ) {
-    $cmd_dump .= " -p'" . $conf->{db}->{pass} . "'";
+    $cmd_dump .= " -p'" . $src_conf->{db}->{pass} . "'";
 } else {
     $cmd_dump .= ' -p';
 }
-$cmd_dump .= ' ' . $conf->{db}->{name};
+$cmd_dump .= ' ' . $src_conf->{db}->{name};
 $cmd_dump .= ' --tables ' . $tables;
 $cmd_dump .= ' > ' . $dump_fpath;
 
 
 #print "cmd_dump: '$cmd_dump'\n";
 
-print "Dumping tables '$tables' from database '$conf->{db}->{name}' to '$dump_fpath':\n";
-my $dump_msg = "Enter source database password for user '$conf->{db}->{user}': ";
+print "Dumping tables '$tables' from database '$src_conf->{db}->{name}' to '$dump_fpath':\n";
+my $dump_msg = "Enter source database password for user '$src_conf->{db}->{user}': ";
 my $dump_rc = TapTinder::Utils::Cmd::run_cmd_ipc( $cmd_dump, $noipc, $dump_msg );
