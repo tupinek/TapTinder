5f4a268ecd48aaadae328644f5b01d964e6bf5b8
Michal Jurosz <mj@mj41.cz>
2009-05-12 22:14:34 +0000

    server - new utils/rm_msession_data.pl to delete all msession_id or machine_id data and files from database

---
+++ server/utils/rm_msession_data.pl
@@ -0,0 +1,154 @@
+#!perl
+
+use strict;
+use warnings;
+use Carp qw(carp croak verbose);
+use FindBin qw($RealBin);
+
+use Getopt::Long;
+use Pod::Usage;
+
+use Data::Dumper;
+use File::Spec::Functions;
+
+use lib "$FindBin::Bin/../lib";
+use TapTinder::DB;
+use TapTinder::Utils::Conf qw(load_conf_multi);
+use TapTinder::Utils::DB qw(get_connected_schema);
+
+my $help = 0;
+my $ver = 2;
+my $sel_msession_id = undef;
+my $sel_machine_id = undef;
+my $options_ok = GetOptions(
+    'help|h|?' => \$help,
+    'ver|v=i' => \$ver,
+    'msession_id=i' => \$sel_msession_id,
+    'machine_id=i' => \$sel_machine_id,
+);
+pod2usage(1) if $help || !$options_ok;
+if ( (not defined $sel_machine_id) && (not defined $sel_msession_id) ) {
+    print "No machine_id or msession_id selected.\n";
+    pod2usage(1);
+}
+
+my $conf = load_conf_multi( undef, 'db' );
+croak "Configuration for database is empty.\n" unless $conf->{db};
+
+my $schema = get_connected_schema( $conf->{db} );
+
+my $search_cond = {};
+$search_cond->{'msession_id.msession_id'} = $sel_msession_id if defined $sel_msession_id;
+$search_cond->{'msession_id.machine_id'}  = $sel_machine_id  if defined $sel_machine_id;
+
+my $search_attrs = {};
+
+$search_attrs = { 'alias' => 'msession_id', };
+my $rs_msession = $schema->resultset('msession')->search( $search_cond, $search_attrs );
+
+my $found = 0;
+while ( my $row = $rs_msession->next ) {
+    $found = 1;
+}
+
+unless ( $found ) {
+    print "No msessions found for ";
+    print "sel_msession_id=$sel_msession_id" if defined $sel_msession_id;
+    if ( defined $sel_machine_id ) {
+        print ", " if defined $sel_msession_id;
+        print "machine_id=$sel_machine_id";
+    }
+    print ".\n";
+    exit;
+}
+
+
+sub get_rs_for_my_attrs {
+    my ( $schema, $search_cond, $conf ) = @_;
+
+    my ( $table_name, $joins ) = @$conf;
+    print "deleting from table: $table_name\n";
+    $search_attrs = { alias => $table_name.'_id' };
+    $search_attrs->{prefetch} = $joins if defined $joins;
+    my $rs = $schema->resultset($table_name)->search( $search_cond, $search_attrs );
+}
+
+sub delete_fsfile {
+    my ( $schema, $fsfile_id ) = @_;
+
+    my $table_name = 'fsfile';
+    my $row = $schema->resultset($table_name)->find(
+        { 'fsfile_id.fsfile_id' => $fsfile_id, },
+        {
+            'select' => [ 'fsfile_id.fsfile_id', 'fspath_id.path', 'fsfile_id.name' ],
+            'as' =>     [ 'fsfile_id', 'file_path', 'file_name' ],
+            join => 'fspath_id',
+            alias => $table_name.'_id',
+        }
+    );
+
+    my $full_fpath = catfile( $row->get_column('file_path'), $row->get_column('file_name') );
+    print "  deleting: '$full_fpath'\n";
+    unlink( $full_fpath );
+    $row->delete;
+    return 1;
+}
+
+
+my $files_conf = [ 'msjobp_cmd', [ 'output_id', 'outdata_id', { 'msjobp_id' => { 'msjob_id' => 'msession_id' }, }, ], ];
+my $rs = get_rs_for_my_attrs( $schema, $search_cond, $files_conf );
+
+# Prepare list of files to delete.
+my @fsfile_ids_to_delete = ();
+while ( my $row = $rs->next ) {
+    my $data = { $row->get_columns };
+    if ( $data->{'output_id'} ) {
+        push @fsfile_ids_to_delete, $data->{'output_id'};
+    }
+    if ( $data->{'outdata_id'} ) {
+        push @fsfile_ids_to_delete, $data->{'outdata_id'};
+    }
+}
+
+my $all_confs = [
+    [ 'ttest', { 'trun_id' => { 'msjobp_cmd_id' => { 'msjobp_id' => { 'msjob_id' => 'msession_id' }, }, }, }, ],
+    [ 'trun', { 'msjobp_cmd_id' => { 'msjobp_id' => { 'msjob_id' => 'msession_id' }, }, }, ],
+    [ 'msjobp_cmd', { 'msjobp_id' => { 'msjob_id' => 'msession_id' }, }, ],
+    [ 'msjobp', { 'msjob_id' => 'msession_id' }, ],
+    [ 'msjob', 'msession_id', ],
+    [ 'mslog', 'msession_id', ],
+    [ 'msession', undef ],
+];
+
+# Delete direct data.
+foreach my $conf_num ( 0..$#$all_confs ) {
+    my $rs = get_rs_for_my_attrs( $schema, $search_cond, $all_confs->[ $conf_num ] );
+    $rs->delete_all;
+}
+
+# Delete files.
+print "deleting from table: fsfile\n";
+foreach my $fsfile_id ( @fsfile_ids_to_delete ) {
+    delete_fsfile( $schema, $fsfile_id );
+}
+
+
+=head1 NAME
+
+rm_msession_data.pl - Remove all data and files related to one or all machine sessions.
+
+=head1 SYNOPSIS
+
+perl tests-to-db.pl [options]
+
+ Options:
+   --help
+   --ver=$NUM .. Verbosity level. Default 2.
+   --msession_id=$ID .. Machine session id.
+   --machine_id=$ID .. Machine id.
+
+=head1 DESCRIPTION
+
+B<This program> will delete ..
+
+=cut
