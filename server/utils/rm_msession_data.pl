e60f708fe42cc7b27eb4187b688af1a76dbbdff3
Michal Jurosz <mj@mj41.cz>
2009-05-27 18:02:38 +0000

    server * delete from tfile * add new options trun_id and msjob_id

--- server/utils/rm_msession_data.pl
+++ server/utils/rm_msession_data.pl
@@ -20,15 +20,24 @@ my $help = 0;
 my $ver = 2;
 my $sel_msession_id = undef;
 my $sel_machine_id = undef;
+my $sel_msjob_id = undef;
+my $sel_trun_id = undef;
 my $options_ok = GetOptions(
     'help|h|?' => \$help,
     'ver|v=i' => \$ver,
+    'trun_id=i' => \$sel_trun_id,
+    'msjob_id=i' => \$sel_msjob_id,
     'msession_id=i' => \$sel_msession_id,
     'machine_id=i' => \$sel_machine_id,
 );
 pod2usage(1) if $help || !$options_ok;
-if ( (not defined $sel_machine_id) && (not defined $sel_msession_id) ) {
-    print "No machine_id or msession_id selected.\n";
+if (    (not defined $sel_machine_id)
+     && (not defined $sel_msession_id)
+     && (not defined $sel_msjob_id)
+     && (not defined $sel_trun_id)
+   )
+{
+    print "No machine_id, msession_id, msjob_id or trun_id selected.\n";
     pod2usage(1);
 }
 
@@ -38,13 +47,24 @@ croak "Configuration for database is empty.\n" unless $conf->{db};
 my $schema = get_connected_schema( $conf->{db} );
 
 my $search_cond = {};
+$search_cond->{'trun_id.trun_id'}         = $sel_trun_id if defined $sel_trun_id;
+$search_cond->{'msjob_id.msjob_id'}       = $sel_msjob_id if defined $sel_msjob_id;
 $search_cond->{'msession_id.msession_id'} = $sel_msession_id if defined $sel_msession_id;
 $search_cond->{'msession_id.machine_id'}  = $sel_machine_id  if defined $sel_machine_id;
 
 my $search_attrs = {};
+my $table_name;
 
-$search_attrs = { 'alias' => 'msession_id', };
-my $rs_msession = $schema->resultset('msession')->search( $search_cond, $search_attrs );
+if ( defined $sel_trun_id ) {
+    $table_name = 'trun';
+} elsif ( defined $sel_msjob_id ) {
+    $table_name = 'msjob';
+} else {
+    $table_name = 'msession';
+}
+
+$search_attrs = { 'alias' => $table_name.'_id', };
+my $rs_msession = $schema->resultset($table_name)->search( $search_cond, $search_attrs );
 
 my $found = 0;
 while ( my $row = $rs_msession->next ) {
@@ -52,10 +72,22 @@ while ( my $row = $rs_msession->next ) {
 }
 
 unless ( $found ) {
-    print "No msessions found for ";
-    print "sel_msession_id=$sel_msession_id" if defined $sel_msession_id;
+    print "No records found in table $table_name for ";
+
+    my $id_str = '';
+    if ( defined $sel_trun_id ) {
+        print "trun_id=$sel_trun_id";
+    }
+    if ( defined $sel_msjob_id ) {
+        print ", " if $id_str;
+        print "msjob_id=$sel_msjob_id";
+    }
+    if ( defined $sel_msession_id ) {
+        print ", " if $id_str;
+        print "msession_id=$sel_msession_id";
+    }
     if ( defined $sel_machine_id ) {
-        print ", " if defined $sel_msession_id;
+        print ", " if $id_str;
         print "machine_id=$sel_machine_id";
     }
     print ".\n";
@@ -95,30 +127,67 @@ sub delete_fsfile {
 }
 
 
-my $files_conf = [ 'msjobp_cmd', [ 'output_id', 'outdata_id', { 'msjobp_id' => { 'msjob_id' => 'msession_id' }, }, ], ];
-my $rs = get_rs_for_my_attrs( $schema, $search_cond, $files_conf );
-
-# Prepare list of files to delete.
 my @fsfile_ids_to_delete = ();
-while ( my $row = $rs->next ) {
-    my $data = { $row->get_columns };
-    if ( $data->{'output_id'} ) {
-        push @fsfile_ids_to_delete, $data->{'output_id'};
-    }
-    if ( $data->{'outdata_id'} ) {
-        push @fsfile_ids_to_delete, $data->{'outdata_id'};
+unless ( defined $sel_trun_id ) {
+    my $files_conf = [ 'msjobp_cmd', [ 'output_id', 'outdata_id', { 'msjobp_id' => { 'msjob_id' => 'msession_id' }, }, ], ];
+    my $rs = get_rs_for_my_attrs( $schema, $search_cond, $files_conf );
+
+    # Prepare list of files to delete.
+    while ( my $row = $rs->next ) {
+        my $data = { $row->get_columns };
+        if ( $data->{'output_id'} ) {
+            push @fsfile_ids_to_delete, $data->{'output_id'};
+        }
+        if ( $data->{'outdata_id'} ) {
+            push @fsfile_ids_to_delete, $data->{'outdata_id'};
+        }
     }
 }
 
 my $all_confs = [
-    [ 'ttest', { 'trun_id' => { 'msjobp_cmd_id' => { 'msjobp_id' => { 'msjob_id' => 'msession_id' }, }, }, }, ],
-    [ 'trun', { 'msjobp_cmd_id' => { 'msjobp_id' => { 'msjob_id' => 'msession_id' }, }, }, ],
-    [ 'msjobp_cmd', { 'msjobp_id' => { 'msjob_id' => 'msession_id' }, }, ],
-    [ 'msjobp', { 'msjob_id' => 'msession_id' }, ],
-    [ 'msjob', 'msession_id', ],
-    [ 'mslog', 'msession_id', ],
-    [ 'msession', undef ],
+    [
+        'ttest',
+        { 'trun_id' => { 'msjobp_cmd_id' => { 'msjobp_id' => { 'msjob_id' => 'msession_id' }, }, }, },
+    ], [
+        'tfile',
+        { 'trun_id' => { 'msjobp_cmd_id' => { 'msjobp_id' => { 'msjob_id' => 'msession_id' }, }, }, },
+    ], [
+        'trun',
+        { 'msjobp_cmd_id' => { 'msjobp_id' => { 'msjob_id' => 'msession_id' }, }, },
+    ],
 ];
+
+
+unless ( defined $sel_trun_id ) {
+    # del from  msjob related tables
+    $all_confs = [
+        @$all_confs,
+        [
+            'msjobp_cmd',
+            { 'msjobp_id' => { 'msjob_id' => 'msession_id' }, },
+        ], [
+            'msjobp',
+            { 'msjob_id' => 'msession_id' },
+        ], [
+            'msjob',
+            'msession_id',
+        ],
+    ];
+
+    # del from msession related tables
+    unless ( defined $sel_msjob_id ) {
+        $all_confs = [
+            @$all_confs,
+            [
+                'mslog',
+                'msession_id',
+            ], [
+                'msession',
+                undef
+            ],
+        ];
+    }
+}
 
 # Delete direct data.
 foreach my $conf_num ( 0..$#$all_confs ) {
@@ -126,10 +195,13 @@ foreach my $conf_num ( 0..$#$all_confs ) {
     $rs->delete_all;
 }
 
-# Delete files.
-print "deleting from table: fsfile\n";
-foreach my $fsfile_id ( @fsfile_ids_to_delete ) {
-    delete_fsfile( $schema, $fsfile_id );
+
+unless ( defined $sel_trun_id ) {
+    # Delete files.
+    print "deleting from table: fsfile\n";
+    foreach my $fsfile_id ( @fsfile_ids_to_delete ) {
+        delete_fsfile( $schema, $fsfile_id );
+    }
 }
 
 
@@ -144,6 +216,8 @@ perl tests-to-db.pl [options]
  Options:
    --help
    --ver=$NUM .. Verbosity level. Default 2.
+   --trun_id=$ID .. Machine session test run id.
+   --msjob_id=$ID .. Machine session job id.
    --msession_id=$ID .. Machine session id.
    --machine_id=$ID .. Machine id.
 
