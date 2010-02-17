86291efd8c6b1ebf0f64fe8e08b0092be4c70ae6
Michal Jurosz <mj@mj41.cz>
2010-02-17 23:52:08 +0000

    server - fix loop-dev.sh

--- server/cron/loop-dev.sh
+++ server/cron/loop-dev.sh
@@ -2,9 +2,9 @@ for ((i=1;1;i++)); do
 echo "Run number: " $i
 date
 echo ""
-nice -n 10 perl repository-update.pl -p TapTinder-tr1
-nice -n 10 perl repository-update.pl -p TapTinder-tr2
-nice -n 10 perl repository-update.pl -p TapTinder-tr3
+nice -n 10 perl repository-update.pl --project TapTinder-tr1
+nice -n 10 perl repository-update.pl --project TapTinder-tr2
+nice -n 10 perl repository-update.pl --project TapTinder-tr3
 echo "Sleeping for 300 s ..."
 sleep 300
 echo ""
