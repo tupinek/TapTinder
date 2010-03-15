5f3f6d7fc4dbcac5b49f9ce6948b94e6e00f5ca0
Michal Jurosz <mj@mj41.cz>
2010-03-15 16:47:02 +0000

    new start scripts finished

--- server/utils/start-server.sh
+++ server/utils/start-server.sh
@@ -3,7 +3,7 @@
 function echo_help {
 cat <<USAGE_END
 Usage:
-  utils/start-server.sh dev|copy|prod d|dp|f
+  utils/start-server.sh dev|copy|prod d|dp|f start|stop
 
   dev ... devel
   copy ... copy of production
@@ -15,73 +15,70 @@ Usage:
 
 Example:
   utils/start-server.sh dev d
-  utils/start-server.sh dev p
+  utils/start-server.sh dev f start
+  utils/start-server.sh dev f stop
 
 USAGE_END
 }
 
+function start_fastcgi {
+    TYPE=$1
+    CMD=$2
+    CMD_PAR=$3
+    PORT_DEV=$4
+    PORT_FCGI=$5
+    PIDFNAME=$6
 
-if [ "$1" == "dev" ]; then
-    export CATALYST_DEBUG=1
-    export DBIC_TRACE=1
-
-    if [ "$2" == "d" -o "$2" == "dp" ]; then 
-        if [ "$2" = "dp" ]; then
+    if [ "$CMD" == "d" -o "$CMD" == "dp" ]; then 
+        # devel debugging
+        if [ "$TYPE" == "dev" ]; then
+            export DBIC_TRACE=1
+            export CATALYST_DEBUG=1
+        fi
+    
+        if [ "$CMD" == "dp" ]; then
           export CATALYST_ENGINE='HTTP::Prefork'
         fi 
-        export CATALYST_PORT=2000
-        perl script/taptinder_web_server.pl -r
+        perl script/taptinder_web_server.pl -r -p $PORT_DEV
         exit
     fi
-
-    if [ "$2" == "f" ]; then 
-        perl script/taptinder_web_fastcgi.pl -l :3000 -n 2 -p temp/ttdev.pid -d
-        exit
+    
+    
+    if [ "$CMD" == "f" ]; then 
+        if [ "$CMD_PAR" == "start" ]; then 
+            perl script/taptinder_web_fastcgi.pl -l :$PORT_FCGI -n 2 -p $PIDFNAME -d
+            exit
+        fi
+ 
+        if [ "$CMD_PAR" == "stop" ]; then 
+            if [ -f "$PIDFNAME" ]; then
+                kill `cat $PIDFNAME`
+            else
+                echo "Can't find file '$PIDFNAME'."
+            fi
+            exit
+        fi
     fi
 
     echo_help
     exit
+}
+
+TYPE="$1"
+CMD="$2"
+CMD_PAR="$3"
+
+if [ "$TYPE" == "dev" ]; then
+
+    start_fastcgi "$TYPE" "$CMD" "$CMD_PAR" 2000 3000 "temp/ttdev.pid"
 fi
 
-if [ "$1" == "copy" ]; then
-    if [ "$2" == "d" -o "$2" == "dp" ]; then 
-        if [ "$2" = "dp" ]; then
-          export CATALYST_ENGINE='HTTP::Prefork'
-        fi 
-        export DBIC_TRACE=1
-        export CATALYST_PORT=2000
-        perl script/taptinder_web_server.pl -r -d
-        exit
-    fi
-
-    if [ "$2" == "f" ]; then 
-        perl script/taptinder_web_fastcgi.pl -l :4000 -n 2 -p temp/ttcopy.pid -d
-        exit
-    fi
-
-    echo_help
-    exit
+if [ "$TYPE" == "copy" ]; then
+    start_fastcgi "$TYPE" "$CMD" "$CMD_PAR" 2000 4000 "temp/ttcopy.pid"
 fi
 
-
-if [ "$1" == "prod" ]; then
-    if [ "$2" == "d" -o "$2" == "dp" ]; then 
-        if [ "$2" = "dp" ]; then
-          export CATALYST_ENGINE='HTTP::Prefork'
-        fi 
-        export DBIC_TRACE=1
-        export CATALYST_PORT=2000
-        perl script/taptinder_web_server.pl -r -d
-        exit
-    fi
-
-    if [ "$2" == "f" ]; then 
-        perl script/taptinder_web_fastcgi.pl -l :5000 -n 2 -p temp/tt.pid -d
-        exit
-    fi
-
-    echo_help
-    exit
+if [ "$TYPE" == "prod" ]; then
+    start_fastcgi "$TYPE" "$CMD" "$CMD_PAR" 2000 5000 "temp/tt.pid"
 fi
 
 
