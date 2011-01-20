3c486cd6046f75f7d5648ed763360f5f1cd39720
Michal Jurosz <mj@mj41.cz>
2011-01-20 22:16:42 +0100

    server - update loop scripts

--- server/cron/loop-dev.sh
+++ server/cron/loop-dev.sh
@@ -3,9 +3,13 @@ echo "Run number: " $i
 date
 echo ""
 nice -n 10 perl repository-update.pl --project tt-tr1
+sleep 5
 nice -n 10 perl repository-update.pl --project tt-tr2
+sleep 5
 nice -n 10 perl repository-update.pl --project tt-tr3
+sleep 5
 nice -n 10 perl repository-update.pl --project parrot
+sleep 5
 nice -n 10 perl repository-update.pl --project rakudo
 echo "Sleeping for 300 s ..."
 sleep 300
--- server/cron/loop.sh
+++ server/cron/loop.sh
@@ -5,15 +5,17 @@ echo ""
 
 echo "Repository update:"
 nice -n 10 perl repository-update.pl --project=parrot
+sleep 5
+nice -n 10 perl repository-update.pl --project=rakudo
 echo ""
 
-echo "Tests to DB:"
-nice -n 10 perl tests-to-db.pl --limit=10 --ver=2
-echo ""
+#echo "Tests to DB:"
+#nice -n 10 perl tests-to-db.pl --limit=10 --ver=2
+#echo ""
 
-echo "Sleeping for 5 s ..."
-
+echo "Sleeping for 30 s ..."
 sleep 30
+
 echo ""
 echo ""
 done
