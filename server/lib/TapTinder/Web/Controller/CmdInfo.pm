d86d8e1abfeb4093e18143385452147586232cd4
Michal Jurosz <mj@mj41.cz>
2011-02-15 19:48:26 +0100

    CmdInfo - show all IDs

--- server/lib/TapTinder/Web/Controller/CmdInfo.pm
+++ server/lib/TapTinder/Web/Controller/CmdInfo.pm
@@ -59,47 +59,52 @@ sub index : Path  {
                 { 'outdata_id' => 'fspath_id', },
             ],
             'select' => [qw/
-                me.msjobp_cmd_id
+                me.msjobp_cmd_id        me.jobp_cmd_id
                 me.start_time           me.end_time
                 cmd_id.name             jobp_cmd_id.params      jobp_cmd_id.rorder
                 status_id.name          status_id.descr
-                output_id.name          fspath_id.web_path      fspath_id.path
-                outdata_id.name         fspath_id_2.web_path    
+                output_id.fsfile_id     output_id.fspath_id     output_id.name
+                fspath_id.web_path      fspath_id.path
+                outdata_id.fsfile_id    outdata_id.name         fspath_id_2.web_path    
                 author_id.rauthor_id    author_id.rep_login     user_id.irc_nick
                 committer_id.rauthor_id committer_id.rep_login  user_id_2.irc_nick
-                rep_id.name             rep_id.github_url
+                rep_id.rep_id           rep_id.name             rep_id.github_url
                 project_id.project_id   project_id.name
                 project_id.url          project_id.descr
                 msjobp_id.start_time    msjobp_id.end_time      
                 msjob_id.msjob_id       msjob_id.start_time     msjob_id.end_time
                 msproc_id.start_time    msproc_id.end_time      
                 abort_reason_id.name    abort_reason_id.descr
-                rcommit_id.msg
+                rcommit_id.rcommit_id   rcommit_id.msg          rcommit_id.sha_id
                 sha_id.sha              rcommit_id.author_time  rcommit_id.committer_time
                 machine_id.machine_id   machine_id.name         machine_id.descr
                 machine_id.cpuarch      machine_id.osname       machine_id.archname
+                msession_id.msession_id msproc_id.msproc_id
+                job_id.job_id           msjobp_id.jobp_id
             /],
             'as' => [qw/ 
-                msjobp_cmd_id
+                msjobp_cmd_id           jobp_cmd_id
                 cmd_start_time          cmd_end_time
                 cmd_name                cmd_params              cmd_order
                 status_name             status_descr
-                output_fname            output_fwpath           output_fspath
-                outdata_fname           outdata_fwpath
+                output_fsfile_id        output_fspath_id        output_fname
+                output_fwpath           output_fspath
+                outdata_id              outdata_fname           outdata_fwpath
                 author_rauthor_id       author_login            author_irc_nick
                 committer_rauthor_id    committer_login         commitrer_irc_nick
-                rep_name                rep_github_url
+                rep_id                  rep_name                rep_github_url
                 project_id              project_name            
                 project_url             project_descr
                 msjobp_start_time       msjobp_end_time
                 msjob_id                msjob_start_time        msjob_end_time
                 msproc_start_time       msproc_end_time
                 msproc_areason_name     msproc_areason_descr
-                rcommit_msg
+                rcommit_id              rcommit_msg             sha_id
                 rcommit_sha             rcommit_author_date     rcommit_committer_date
                 machine_id              machine_name            machine_descr
                 machine_cpuarch         machine_osname          machine_archname
-                
+                msession_id             msproc_id
+                job_id                  jobp_id
             /],
         }
     );
